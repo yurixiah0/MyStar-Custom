0d7581bf55b1418768f7444c690989bbf39ac397
yurixiah0 <yurika.watanabe0424@gmail.com>
2024-08-13 14:38:56 +0900

    ログイン画面の追加

--- mystarcustom/mystarcustom/urls.py
+++ mystarcustom/mystarcustom/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import TopView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", TopView.as_view())
 ]
